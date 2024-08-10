128f45fc9feb42b751a0a08e6b223f388b1c0aa7
alex9609 <alrodriguez41@poligran.edu.co>
2024-08-09 22:41:34 -0500

    feat: Añadiendo funcionalidad de API gemini

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+//import 'package:path_provider/path_provider.dart';
+import 'package:external_path/external_path.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:visual_ia/interfaz.dart';
 import 'package:visual_ia/gemini.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 
 late List<CameraDescription> _cameras;
 
@@ -14,7 +14,6 @@ void main() async {
   await Permission.camera.request();
   await Permission.manageExternalStorage.request();
   _cameras = await availableCameras();
-  Gemini.init(apiKey: 'mi api');
   runApp(const Camara());
 }
 
@@ -65,21 +64,33 @@ class _EstadoCamara extends State<Camara> {
   }
 
   Future<File?> guardarImagenGaleria(XFile imageFile) async {
-    // Verificar permiso de almacenamiento
-    final status = await Permission.manageExternalStorage.request();//Permission.storage.request();
-    if (status.isGranted) {
-      final directory = await getApplicationDocumentsDirectory();
-      final newPath = directory.path + '/ImagenesVIA';
-      final carpeta = Directory(newPath);
-      await carpeta.create(recursive: true); // Crear directorio si no existe
+    // // Verificar permiso de almacenamiento
+    // final status = await Permission.manageExternalStorage.request();//Permission.storage.request();
+    // if (true) {
+    // //if (status.isGranted) {
+    //   final directory = await getApplicationDocumentsDirectory();
+    //   final newPath = directory.path + '/ImagenesVIA';
+    //   final carpeta = Directory(newPath);
+    //   await carpeta.create(recursive: true); // Crear directorio si no existe
 
-      final file = File('${carpeta.path}/${DateTime.now().millisecondsSinceEpoch}.jpg');
-      await file.writeAsBytes(await imageFile.readAsBytes());
-      return file; // Retornar el archivo guardado para posibles acciones
-    } else {
-      print('Permiso de almacenamiento denegado');
-      return null;
-    }
+    //   final file = File('${carpeta.path}/${DateTime.now().millisecondsSinceEpoch}.jpg');
+    //   await file.writeAsBytes(await imageFile.readAsBytes());
+    //   return file; // Retornar el archivo guardado para posibles acciones
+    // } else {
+    //   print('Permiso de almacenamiento denegado');
+    //   return null;
+    // }
+
+      final downlaodPath = await ExternalPath.getExternalStoragePublicDirectory(
+      ExternalPath.DIRECTORY_DOWNLOADS);
+      final fileName = '${DateTime.now().millisecondsSinceEpoch}.png';
+      final file = File('$downlaodPath/$fileName');
+
+      try {
+        await file.writeAsBytes(await imageFile.readAsBytes());
+      } catch (_) {}
+
+      return file;
   }
 
   @override
